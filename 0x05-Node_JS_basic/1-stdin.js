d416c6c9cb7a9609be33c746a2853be44e454359
Benjmain Atsu <Bredrenbonish@gmail.com>
2024-05-01 03:28:08 +0000

    Nodejs basics

---
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -0,0 +1,12 @@
+process.stdout.write('Welcome to Holberton School, what is your name?' + '\n');
+
+process.stdin.on('readable', () => {
+    const username = process.stdin.read();
+    if (username ==! null) {
+        process.stdout.write(`Your name is: ${username}`);
+    }
+});
+
+process.stdin.on('end', () => {
+    process.stdout.write('This important software is now closing');
+});
